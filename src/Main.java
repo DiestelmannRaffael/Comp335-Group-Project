26c7c60f38b12a1476638e12dc4f880e12678653
DiestelmannRaffael <raffael@diestelmann.de>
2019-06-02 10:32:19 +1000

    Use first fit if no fitting server found

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,18 @@ import java.util.Collections;
 import java.util.List;
 
 public class Main {
+    private static int cpuCores;
+    private static int disk;
+    private static int memory;
+    private static int jobId;
+    private static JobSchedulerClient client;
+    private static String scheduleInfo = null;
+
     public static void main(String[] args) throws IOException {
         String serverName = "127.0.0.1";
         int port = 8096;
         Socket socket = new Socket(serverName, port);
-        JobSchedulerClient client = new JobSchedulerClient(socket);
+        client = new JobSchedulerClient(socket);
         //XmlReader xmlReader = new XmlReader();
 
         //send the helo
@@ -47,12 +54,12 @@ public class Main {
             String[] parts = temp.split(" ");
 
             int submitTime = Integer.parseInt(parts[1]);
-            int jobId = Integer.parseInt(parts[2]);
+            jobId = Integer.parseInt(parts[2]);
             //System.out.println("JOB ID: " + jobId);
             int estRunTime = Integer.parseInt(parts[3]);
-            int cpuCores = Integer.parseInt(parts[4]);
-            int memory = Integer.parseInt(parts[5]);
-            int disk = Integer.parseInt(parts[6]);
+             cpuCores = Integer.parseInt(parts[4]);
+             memory = Integer.parseInt(parts[5]);
+             disk = Integer.parseInt(parts[6]);
 
             DynamicJob dynamicJob = new DynamicJob(submitTime, jobId, estRunTime, cpuCores, memory, disk);
             dynamicJobList.add(dynamicJob);
@@ -89,30 +96,11 @@ public class Main {
             }
             initialRun = false;
 
-            String scheduleInfo = null;
-            int i = 0;
-            boolean lastElement = false;
+            
 
             //first fit algorithm
             if (args[0].equals("-a") && args[1].equals("ff")) {
-                for (DynamicServer ds : dynamicServerList) {
-                    if(i++ == dynamicServerList.size() - 1)
-                        lastElement = true;
-                    if (ds.getCpuCores() >= cpuCores && ds.getDisk() >= disk && ds.getMemory() >= memory) {
-                        scheduleInfo = "SCHD " + jobId + " " + ds.getServerType() + " " + ds.getServerTypeId();
-                        client.sendMessageToServer(scheduleInfo);
-                        break;
-                        // no available server found
-                    } else if (lastElement) {
-                        for (DynamicServer is : initialServerList) {
-                            if (is.getCpuCores() >= cpuCores && is.getDisk() >= disk && is.getMemory() >= memory) {
-                                scheduleInfo = "SCHD " + jobId + " " + is.getServerType() + " " + is.getServerTypeId();
-                                client.sendMessageToServer(scheduleInfo);
-                                break;
-                            }
-                        }
-                    }
-                }
+                firstFit(dynamicServerList, initialServerList);
             }
 
             if (args[0].equals("-a") && args[1].equals("new")) {
@@ -125,6 +113,10 @@ public class Main {
                         scheduleInfo = "SCHD" + jobId + " " + ds.getServerType() + " " + ds.getServerTypeId();
                     } else if (ds.getServerState() == 3 && ds.getCpuCores() > cpuCores) { // bootup inactive servers
                         scheduleInfo = "SCHD" + jobId + " " + ds.getServerType() + " " + ds.getServerTypeId();
+                    } else if (ds.getServerState() == 4 && ds.getCpuCores() > cpuCores) { // bootup inactive servers
+                        scheduleInfo = "SCHD" + jobId + " " + ds.getServerType() + " " + ds.getServerTypeId();
+                    } else {
+                        firstFit(dynamicServerList, initialServerList);
                     }
                 }
             }
@@ -144,5 +136,28 @@ public class Main {
             }
         }
         socket.close();
+    }
+
+    private static void firstFit(List<DynamicServer> dynamicServerList, List<DynamicServer> initialServerList) throws IOException {
+        int i = 0;
+        boolean lastElement = false;
+        for (DynamicServer ds : dynamicServerList) {
+            if(i++ == dynamicServerList.size() - 1)
+                lastElement = true;
+            if (ds.getCpuCores() >= cpuCores && ds.getDisk() >= disk && ds.getMemory() >= memory) {
+                scheduleInfo = "SCHD " + jobId + " " + ds.getServerType() + " " + ds.getServerTypeId();
+                client.sendMessageToServer(scheduleInfo);
+                break;
+                // no available server found
+            } else if (lastElement) {
+                for (DynamicServer is : initialServerList) {
+                    if (is.getCpuCores() >= cpuCores && is.getDisk() >= disk && is.getMemory() >= memory) {
+                        scheduleInfo = "SCHD " + jobId + " " + is.getServerType() + " " + is.getServerTypeId();
+                        client.sendMessageToServer(scheduleInfo);
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
